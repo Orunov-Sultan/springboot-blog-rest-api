98f53760f07b196b9ec5e429f0a823b182d47585
Sultan <osultan@yandex.ru>
2024-12-02 10:32:08 +0500

    Добавил Post Entity

---
+++ src/main/java/com/webdev/blog/v1/entity/Post.java
@@ -0,0 +1,27 @@
+package com.webdev.blog.v1.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "posts", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "title")
+})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String content;
+}
