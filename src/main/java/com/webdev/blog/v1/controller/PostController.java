b63fe5991160f257d7db654e539e46fb6690b509
Sultan <osultan@yandex.ru>
2024-12-02 17:57:30 +0500

    Добавил в сортировку вывода всех постов, вывод по возрастанию и по убыванию

--- src/main/java/com/webdev/blog/v1/controller/PostController.java
+++ src/main/java/com/webdev/blog/v1/controller/PostController.java
@@ -28,10 +28,11 @@ public class PostController {
     public ResponseEntity<PostResponse> getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ) {
 
-        PostResponse posts = postService.getAllPosts(pageNo, pageSize, sortBy);
+        PostResponse posts = postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
         return ResponseEntity.ok(posts);
     }
 
--- src/main/java/com/webdev/blog/v1/service/PostService.java
+++ src/main/java/com/webdev/blog/v1/service/PostService.java
@@ -5,7 +5,7 @@ import com.webdev.blog.v1.dto.PostResponse;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
-    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy);
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
     PostDto getPostById(Long id);
     PostDto updatePost(Long id, PostDto postDto);
     void deletePost(Long id);
