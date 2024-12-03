dd97bc40accd951cea21a4413b3dfb7fb73adea2
Sultan <osultan@yandex.ru>
2024-12-03 14:35:18 +0500

    Добавил CommentService и CommentServiceImpl

---
+++ src/main/java/com/webdev/blog/v1/service/impl/CommentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.webdev.blog.v1.service.impl;
+
+import com.webdev.blog.v1.dto.CommentDto;
+import com.webdev.blog.v1.service.CommentService;
+
+public class CommentServiceImpl implements CommentService {
+
+}
