49ace2ffab464d2951f9ea9abd5fb89ac08d5a47
Sultan <osultan@yandex.ru>
2024-12-04 23:13:48 +0500

    Добавил ErrorDetails

---
+++ src/main/java/com/webdev/blog/v1/exception/ErrorDetails.java
@@ -0,0 +1,20 @@
+package com.webdev.blog.v1.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+    private String path;
+    private int statusCode;
+}
