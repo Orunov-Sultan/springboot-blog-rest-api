105d2c00c7fd7128a716c79d9923cd1e4fbdb387
Sultan <osultan@yandex.ru>
2024-12-13 00:02:50 +0500

    Реализация интерфейса UserDetailsService

---
+++ src/main/java/com/webdev/blog/v1/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.webdev.blog.v1.security;
+
+import com.webdev.blog.v1.entity.User;
+import com.webdev.blog.v1.repostitory.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() ->
+            new UsernameNotFoundException("Пользователь с таким именем или электронной почтой: " + usernameOrEmail + " не найден"));
+
+        Set<GrantedAuthority> authorities = user
+                .getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+}
