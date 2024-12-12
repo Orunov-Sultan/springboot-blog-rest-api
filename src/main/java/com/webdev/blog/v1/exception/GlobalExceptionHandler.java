aa7086669acbc3b2bc11d97cd1aaf830fdc87605
Sultan <osultan@yandex.ru>
2024-12-12 22:57:25 +0500

    Обработал AuthorizationDeniedException исключение

--- src/main/java/com/webdev/blog/v1/exception/GlobalExceptionHandler.java
+++ src/main/java/com/webdev/blog/v1/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -79,5 +81,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errors.put("statusCode", String.valueOf(HttpStatus.BAD_REQUEST.value()));
         
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorDetails> handleAuthorizationDeniedException(AuthorizationDeniedException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage(ex.getMessage());
+        errorDetails.setTimestamp(LocalDateTime.now());
+        errorDetails.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+        errorDetails.setPath(request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 }
