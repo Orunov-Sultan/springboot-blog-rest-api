7f0b80a45f41484ec26806c2b154eeaf9c560a60
Sultan <osultan@yandex.ru>
2024-12-24 14:28:08 +0500

    create jwt authentication filter

--- src/main/java/com/webdev/blog/v1/security/JwtTokenProvider.java
+++ src/main/java/com/webdev/blog/v1/security/JwtTokenProvider.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -53,7 +52,7 @@ public class JwtTokenProvider {
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
-                .getSubject()
+                .getSubject();
     }
 
     // Проверка JWT токена
