65ed1be9529d3b38adaad3e416c1867eb8fa349a
Sultan <osultan@yandex.ru>
2024-12-24 14:31:56 +0500

    create jwt auth response dto

---
+++ src/main/java/com/webdev/blog/v1/dto/JWTAuthResponse.java
@@ -0,0 +1,15 @@
+package com.webdev.blog.v1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWTAuthResponse {
+    private String accessToken;
+    private String tokenType = "Dearer";
+}
