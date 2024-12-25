9c822fbe3583b3e96522b2e01dd7bc631813f37b
Sultan <osultan@yandex.ru>
2024-12-25 16:07:49 +0500

    Add category service

---
+++ src/main/java/com/webdev/blog/v1/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.webdev.blog.v1.service;
+
+import com.webdev.blog.v1.dto.CategoryDto;
+
+public interface CategoryService {
+    CategoryDto addCategory(CategoryDto categoryDto);
+}
---
+++ src/main/java/com/webdev/blog/v1/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,24 @@
+package com.webdev.blog.v1.service.impl;
+
+import com.webdev.blog.v1.dto.CategoryDto;
+import com.webdev.blog.v1.repostitory.CategoryRepository;
+import com.webdev.blog.v1.service.CategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryRepository categoryRepository;
+    private ModelMapper modelMapper;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper) {
+        this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public CategoryDto addCategory(CategoryDto categoryDto) {
+        return null;
+    }
+}
