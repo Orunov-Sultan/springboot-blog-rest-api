7986accb740e777fbfe03783fb4d5fa30b146fd5
Sultan <osultan@yandex.ru>
2024-12-25 16:16:57 +0500

    Add category controller and add category andpoint

--- src/main/java/com/webdev/blog/v1/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/webdev/blog/v1/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.webdev.blog.v1.service.impl;
 
 import com.webdev.blog.v1.dto.CategoryDto;
+import com.webdev.blog.v1.entity.Category;
 import com.webdev.blog.v1.repostitory.CategoryRepository;
 import com.webdev.blog.v1.service.CategoryService;
 import org.modelmapper.ModelMapper;
@@ -19,6 +20,8 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDto addCategory(CategoryDto categoryDto) {
-        return null;
+        Category category = modelMapper.map(categoryDto, Category.class);
+        Category savedCategory = categoryRepository.save(category);
+        return modelMapper.map(savedCategory, CategoryDto.class);
     }
 }
